0a9c463256b1f4342c0461cc9c91b8ba5d5c01b5
aqibmehedi007 <aqibmehedi007@gmail.com>
2026-02-21 01:33:14 +0600

    fix: explicit production mode in server.js to bypass turbopack symlink issues

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-const dev = process.env.NODE_VALUE !== 'production'
+const dev = process.env.NODE_ENV === 'development'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
